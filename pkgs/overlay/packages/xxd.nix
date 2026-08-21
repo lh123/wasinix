50157d33218d1b03792c67b1d45b0263c8f7bc6b
lh123 <1585086582@qq.com>
2026-08-21 11:43:09 +0800

    fix: patch the xxd provider

--- pkgs/overlay/packages/xxd.nix
+++ pkgs/overlay/packages/xxd.nix
@@ -2,15 +2,29 @@
   prev,
   helpers,
   ...
-}:
-helpers.wasmRename {wasmName = "xxd";} (
-  helpers.libTweaks {
-    passthru.wasix.shipped = true;
+}: let
+  oldProvider = prev.xxd.provider;
+  provider = helpers.libTweaks {
     postPatch = ''
       substituteInPlace Makefile \
         --replace-fail '-fPIC ' "" \
         --replace-fail ' -fno-plt -Wl,-z,now' ""
     '';
+  } oldProvider;
+  xxd = prev.xxd.overrideAttrs (old: {
+    buildCommand =
+      builtins.replaceStrings
+      ["${oldProvider}"]
+      ["${provider}"]
+      old.buildCommand
+      + ''
+        cp --dereference "$out/bin/xxd" "$out/bin/xxd.wasm"
+        rm "$out/bin/xxd"
+      '';
+    passthru = (old.passthru or {}) // {inherit provider;};
+  });
+in
+  helpers.libTweaks {
+    passthru.wasix.shipped = true;
   }
-  prev.xxd
-)
+  xxd
